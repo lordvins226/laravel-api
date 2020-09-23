d1d2be232fb2dcbafd1734792b7d0e43bb2aa48d
lordvins226 <kevineilboudo54@gmail.com>
2020-09-23 10:32:41 +0000

    :hatching_chick: initial commit

---
+++ app/Http/Controllers/TopicalityController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Topicality;
+use Illuminate\Http\Request;
+
+class TopicalityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+     return  Topicality::all();
+        // return $topicalities ->toJson(JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Topicality $topicality)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Topicality $topicality)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Topicality $topicality)
+    {
+        //
+    }
+}
